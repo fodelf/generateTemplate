d6d9008aaa2a0ecb5076c379325b9344334732b3
吴文周 <wuwenzhou@qq.com.cn>
2019-11-14 23:35:26 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<style lang ='css'>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -19,14 +19,12 @@
 
 #nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+}
+a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ webpack.dll.conf.js
@@ -0,0 +1,47 @@
+/*
+ * @Description:Dll配置信息
+ * @Author: 吴文周
+ * @Github: https://github.com/fodelf
+ * @Date: 2019-06-04 17:39:53
+ * @LastEditors: 吴文周
+ * @LastEditTime: 2019-11-14 23:16:16
+ */
+const path = require('path')
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+// dll文件存放的目录
+const dllPath = 'public/vendor'
+
+module.exports = {
+  entry: {
+    // 需要提取的库文件
+    vendor: ['vue', 'vue-router', 'vuex', 'axios', 'element-ui']
+  },
+  output: {
+    path: path.join(__dirname, dllPath),
+    filename: '[name].dll.js',
+    // vendor.dll.js中暴露出的全局变量名
+    // 保持与 webpack.DllPlugin 中名称一致
+    library: '[name]_[hash]'
+  },
+  plugins: [
+    // 清除之前的dll文件
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [path.join(__dirname, dllPath)]
+    }),
+    // 设置环境变量
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'production'
+      }
+    }),
+    // manifest.json 描述动态链接库包含了哪些内容
+    new webpack.DllPlugin({
+      path: path.join(__dirname, dllPath, '[name]-manifest.json'),
+      // 保持与 output.library 中名称一致
+      name: '[name]_[hash]',
+      context: process.cwd()
+    })
+  ]
+}
